85f44fa25963ab1951911013c58a69bddac9d8b3
Flyte <flyte@failcode.co.uk>
2015-11-10 19:33:16 +0000

    Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def required(fname):
 
 config = {
     "name": "django-generic-counter",
-    "version": "0.0.3",
+    "version": "0.0.4",
     "namespace_packages": [],
     "packages": find_packages(exclude=["*.tests",
                                        "*.tests.*",
